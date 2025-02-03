2e1bf3a07bc2c842af8915b8232d6b6acc1fadfe
Azizul Hakim <114043543+Hodako@users.noreply.github.com>
2025-02-03 18:07:46 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const sequelize = require('./config/database');
 const authRoutes = require('./routes/authRoutes');
 const articleRoutes = require('./routes/articleRoutes');
@@ -7,6 +8,7 @@ const articleRoutes = require('./routes/articleRoutes');
 dotenv.config();
 
 const app = express();
+app.use(cors({ origin: '*' })); // Allow all origins for simplicity. Adjust as needed for security.
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 
