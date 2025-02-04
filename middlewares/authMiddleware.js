7934b5ffa8efab659b190503ca30f432ef2ba946
Azizul Hakim <114043543+Hodako@users.noreply.github.com>
2025-02-04 09:16:10 +0600

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/User'); // Ensure that the User model is defined
 
 exports.protect = async (req, res, next) => {
   let token;
