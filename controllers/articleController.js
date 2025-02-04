34124dea89dc4bdca8958c660d5dc5c85b83aa4c
Azizul Hakim <114043543+Hodako@users.noreply.github.com>
2025-02-04 09:15:00 +0600

    Update articleController.js

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -25,3 +25,25 @@ exports.uploadFile = (req, res) => {
   }
   res.status(200).send({ filename: req.file.filename, path: req.file.path });
 };
+
+exports.getArticles = async (req, res) => {
+  try {
+    const articles = await Article.findAll();
+    res.status(200).json(articles);
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to get articles', error: error.message });
+  }
+};
+
+exports.getArticle = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const article = await Article.findByPk(id);
+    if (!article) {
+      return res.status(404).json({ message: 'Article not found' });
+    }
+    res.status(200).json(article);
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to get article', error: error.message });
+  }
+};
